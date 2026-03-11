c84178d0c4352a2c455d0868a8910fccf52527d4
Harsha Vardhan <118455082+PAMUHARSHAVARDHAN@users.noreply.github.com>
2026-03-11 23:34:41 +0530

    Add files via upload

---
+++ Week 4/Day 19/EmpBonuscal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Problem
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your Name ");
+        String Uname = Console.ReadLine();
+
+        Console.WriteLine("Enter your Salary ");
+        int salary = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Your Experience ");
+        int Exp = int.Parse(Console.ReadLine());
+
+        double bonus;
+        
+
+        if (Exp <=2) {
+            bonus = 0.05;
+        }
+        else if (Exp <= 5){
+            bonus = 0.10;
+        }
+        else
+        {
+            bonus = 0.15;
+        }
+        double bonus1 = salary * bonus;
+        double Total = salary + bonus1;
+
+        Console.WriteLine("Name" + Uname);
+        Console.WriteLine("Bonus" + bonus);
+        Console.WriteLine("TotalSalary" + Total);
+
+        Console.ReadLine();
+
+    }
+
+}
+  
