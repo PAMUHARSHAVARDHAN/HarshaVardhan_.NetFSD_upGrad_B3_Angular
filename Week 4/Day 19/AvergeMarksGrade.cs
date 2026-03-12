a1ef0493f7cda749eca249ca239cc6d5c1857d91
Harsha Vardhan <118455082+PAMUHARSHAVARDHAN@users.noreply.github.com>
2026-03-12 23:59:01 +0530

    Add files via upload

---
+++ Week 4/Day 19/AvergeMarksGrade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Student
+{
+    public double CalculateAverage(int m1, int m2, int m3)
+    {
+        return (m1 + m2 + m3) / 3.0;
+    }
+
+    static void Main(string[] args)
+    {
+        Student s = new Student();
+
+        Console.Write("Enter mark 1: ");
+        int m1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter mark 2: ");
+        int m2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter mark 3: ");
+        int m3 = int.Parse(Console.ReadLine());
+
+        double average = s.CalculateAverage(m1, m2, m3);
+
+        string grade;
+
+        if (average >= 90)
+            grade = "A";
+        else if (average >= 80)
+            grade = "B";
+        else if (average >= 70)
+            grade = "C";
+        else if (average >= 60)
+            grade = "D";
+        else
+            grade = "E";
+
+        Console.WriteLine("Average = " + average);
+        Console.WriteLine("Grade = " + grade);
+
+        Console.ReadLine();
+    }
+}
---
+++ Week 4/Day 19/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Calculator
+{
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+    static void Main(string[] args)
+    {
+        Calculator c =new Calculator();
+        int add =c.Add(1,2);
+        int sub =c.Subtract(1,2);
+
+        Console.WriteLine("Addition =" + add);
+        Console.WriteLine("Subtraction =" + sub);
+
+        Console.ReadLine();
+    }
+}
