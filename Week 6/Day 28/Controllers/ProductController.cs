b82a172a2f82b3dc2ba004d541e3222fcafa9003
Harsha Vardhan <118455082+PAMUHARSHAVARDHAN@users.noreply.github.com>
2026-03-26 20:34:55 +0530

    Add files via upload

---
+++ Week 6/Day 28/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product{ Id =1, Name = "Laptop" , Category = "Electronics", Price =45000},
+            new Product{ Id =2, Name = "Shirt" , Category = "Fashion", Price =5000},
+            new Product{ Id =3, Name = "Mobile" , Category = "Electronics", Price =20000},
+            new Product{ Id =4, Name = "watch" , Category = "Accessories", Price =2000}
+        };
+        public IActionResult Index()
+        {
+            return View( products);
+        }
+        public IActionResult Details( )
+        {
+            Product ProObj = new Product() { Id = 3, Name = "Mobile", Category = "Electronics", Price = 20000 };
+
+
+            return View(ProObj);
+        }
+    }
+}
---
+++ Week 6/Day 28/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+         public string Category { get; set; }
+         public int Price { get; set; }
+
+    }
+}
