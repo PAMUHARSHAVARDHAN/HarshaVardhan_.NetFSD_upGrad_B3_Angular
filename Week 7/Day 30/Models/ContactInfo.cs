75328b64aec0b88dab41228ef74168d2307cc729
Harsha Vardhan <118455082+PAMUHARSHAVARDHAN@users.noreply.github.com>
2026-04-01 00:57:31 +0530

    Add files via upload

---
+++ Week 7/Day 30/Models/ContactInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class ContactInfo
+    {
+        [Required (ErrorMessage = "ContactId is required")]
+        public int ContactId { get; set; }
+        [Required (ErrorMessage ="Firstname is required")]
+        [StringLength(15, MinimumLength =5)]
+        public string FirstName {  get; set; }
+        [Required(ErrorMessage = "Lastname is required")]
+        [StringLength(15, MinimumLength = 5)]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Companyname is required")]
+        [StringLength(15, MinimumLength = 5)]
+        public string CompanyName { get; set; }
+        [Required(ErrorMessage = "Mail is required")]
+        [StringLength(20, MinimumLength = 5)]
+        public string EmailId { get; set; }
+        [Required (ErrorMessage ="please enter mobile number")]
+        [Range(100000000,9999999999,ErrorMessage ="please enter 10 digit number")]
+        public long MobileNo { get; set; }
+        [Required(ErrorMessage = "designation is required")]
+        [StringLength(15, MinimumLength = 5)]
+        public string Designation { get; set; }
+
+    }
+}
